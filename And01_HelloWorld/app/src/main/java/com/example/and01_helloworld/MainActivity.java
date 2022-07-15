eac3b245bbd67bbedc4c215ff8d2d8d0e8004115
govl1004 <gkswoals0517@gmail.com>
2022-07-15 17:59:43 +0900

    안드로이드 2022-07-15

---
+++ And01_HelloWorld/app/src/main/java/com/example/and01_helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.and01_helloworld;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
